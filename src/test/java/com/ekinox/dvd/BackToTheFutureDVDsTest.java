d5fa4c0b0e793dbc095530942659894cdab67f4c
Sahar Ellouze <sahar.ellouze10@gmail.com>
2023-11-27 21:15:07 +0100

    DVDs kata : add all different use cases mentioned in the specifications

--- src/test/java/com/ekinox/dvd/BackToTheFutureDVDsTest.java
+++ src/test/java/com/ekinox/dvd/BackToTheFutureDVDsTest.java
@@ -12,4 +12,27 @@ public class BackToTheFutureDVDsTest {
         assertEquals(36, BackToTheFutureDVDs.calculateTotalPrice(input));
     }
 
+    @Test
+    public void testExample2() {
+        String[] input = {"Back to the Future 1", "Back to the Future 3"};
+        assertEquals(27, BackToTheFutureDVDs.calculateTotalPrice(input));
+    }
+
+    @Test
+    public void testExample3() {
+        String[] input = {"Back to the Future 1"};
+        assertEquals(15, BackToTheFutureDVDs.calculateTotalPrice(input));
+    }
+
+    @Test
+    public void testExample4() {
+        String[] input = {"Back to the Future 1", "Back to the Future 2", "Back to the Future 3", "Back to the Future 2"};
+        assertEquals(48, BackToTheFutureDVDs.calculateTotalPrice(input));
+    }
+
+    @Test
+    public void testExample5() {
+        String[] input = {"Back to the Future 1", "Back to the Future 2", "Back to the Future 3", "La chèvre"};
+        assertEquals(56, BackToTheFutureDVDs.calculateTotalPrice(input));
+    }
 }
